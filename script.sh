abcad8231f0665997295f622d2f1f3eda3a8c91c
jero0137 <102569345+jero0137@users.noreply.github.com>
2022-05-12 17:47:31 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
+cd /home/
 git clone https://github.com/jero0137/Contenedores.git
-sudo docker build -t app1:v1 
+sudo docker build -t app1:v1 /Contenedores
 sudo docker run -d -p 80:80 app1:v1 python3 app.py
