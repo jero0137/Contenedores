f9af057298a65f0354620ed30f006c86ceef4595
jero0137 <102569345+jero0137@users.noreply.github.com>
2022-05-13 09:19:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,10 @@
-#llamar librerias
-from dash import Dash, html, dcc  #libreria para servidor web de data science
-import pandas as pd              #libreria para procesar datos
-import plotly.graph_objs as go   #libreria para graficar
+from flask import Flask, render_template
 
-#declarar objetos principales
-app = Dash(__name__)
-#cargar la base de datos
-df = pd.read_excel('data.xlsx')
-#configurar el sitio web
-app.layout = html.Div([html.H1('Programa para ver el covid en AMVA'),
-                       html.Div('Informacion de la pagina, bla bla bla...'),
-                       dcc.Graph(id= 'map',figure={
-                           'data':[{'lat':df['Latitud'],
-                                    'lon':df['Longitud'],
-                                    'marker':{
-                                        'color': 150,
-                                        'size': df['Enfermos'],
-                                        'opacity': 0.6
-                                    },
-                                    'customdata': df['Enfermos'],
-                                    'type':'scattermapbox'}],
-                           'layout':{'mapbox':{
-                           'accesstoken': 'pk.eyJ1IjoibGVvbmFyZG9iZXRhbmN1ciIsImEiOiJjazlybGNiZWcwYjZ6M2dwNGY4MmY2eGpwIn0.EJjpR4klZpOHSfdm7Tsfkw',
-                           'center':{'lat':6.240737,'lon':-75.589900},
-                           'zoom':10},
-                           'hovermode':'closest',
-                           'margin':{'l':0,'r':0,'b':0,'t':0}}                           
-                       })])
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 #funcion principal
 if __name__ == '__main__':
     #cargar el objeto principal a todas las interfaces de red en el puerto 80
