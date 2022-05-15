b3b3104f088d3692152a6ae185642a3802941676
jero0137 <102569345+jero0137@users.noreply.github.com>
2022-05-15 15:21:11 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@ apt install docker-compose -y
 apt install git-core -y
 git clone https://github.com/jero0137/Contenedores.git
 docker build -t app1:v1 Contenedores/
-docker run -d -p 8000:80 app1:v1 python3 app.py
+docker run -d -p 80:80 app1:v1 python3 app.py
